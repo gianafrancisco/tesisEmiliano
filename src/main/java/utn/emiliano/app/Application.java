6b79f3ae28413dd863bb8b7b47162a36c5051698
Francisco Giana <gianafrancisco@gmail.com>
2016-02-07 21:03:43 -0300

    Initial commit

---
+++ src/main/java/utn/emiliano/app/Application.java
@@ -0,0 +1,14 @@
+package utn.emiliano.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by francisco on 03/02/16.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
